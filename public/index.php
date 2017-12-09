a806b2f249aa52dc822a04fd6ee42304597a2420
Micah J. Murray <micah.murray@gmail.com>
2017-12-09 04:16:23 +0000

    updates a comment

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
  */
 
 session_start();
-define('_humblee_start_time', microtime(true)); // for tracking exc time
+define('_humblee_start_time', microtime(true)); // for tracking execution time
 define('_app_server_path', realpath(__DIR__ . '/..') ."/"); // eg: "/home/ubuntu/workspace/myapp/" (note: the '/..' drops the "public" folder off the end )
 define('_app_path', str_replace(rtrim($_SERVER['DOCUMENT_ROOT'],"/"), '', _app_server_path)); // eg: "/myapp/" or just "/" if in root); 
 
