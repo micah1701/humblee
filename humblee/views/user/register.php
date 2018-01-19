93c8b13c9ea3c03545c8ac4f6931d1b497d244e3
Micah J. Murray <micah.murray@gmail.com>
2018-01-19 07:48:20 +0000

    restyles the registration page

--- humblee/views/user/register.php
+++ humblee/views/user/register.php
@@ -8,34 +8,56 @@ if(isset($_SESSION[session_key]['user_id'])) // user is already logged in
 }
 ?>
 
-<h1>Register</h1>
+<h1 class="title">Hello? Who is it?</h1>
+<h2 class="subtitle">Register for access</h2>
+<section class="section columns">
 
-<?php if(isset($error)){ ?>
-<div class="err_message">
-  <ul>
-	<?php foreach($error as $err){ ?>
-	<li><?php echo $err ?></li>
-	<?php } ?>    
-  </ul>
-</div>
-<?php } ?>
+  <form action="<?php echo ( isset($_GET['fwd']) ) ? "?fwd=".$_GET['fwd'] : '' ?>" method="post">
 
-<form action="<?php echo ( isset($_GET['fwd']) ) ? "?fwd=".$_GET['fwd'] : '' ?>" method="post">
+  <?php if(isset($error)){ ?>
+  <div class="field is-two-fifths">
+    <ul class="control">
+  	<?php foreach($error as $err){ ?>
+  	<li class="has-text-danger"><?php echo $err ?></li>
+  	<?php } ?>    
+    </ul>
+  </div>
+  <?php } ?>
 
-  <label for="name">Full Name</label>
-  <input type="text" id="name" name="name">
+  <div class="field is-two-fifths">
+    <label class="label" for="name">Full Name</label> 
+    <div class="control">
+      <input class="input" type="text" id="name" name="name">
+    </div>
+  </div>
   
-  <label for="username">Username</label>
-  <input type="text" id="username" name="username">
+  <div class="field is-two-fifths">
+    <label class="label" for="username">Username</label> 
+    <div class="control">
+      <input class="input" type="text" id="username" name="username">
+    </div>
+  </div>
   
-  <label for="email">E-Mail Address</label>
-  <input type="text" id="email" name="email">
-
-  <label for="password">Create Password</label>
-  <input type="password" id="password" name="password">
-
-  <label for="password_check">Confirm Password</label>
-  <input type="password" id="password_check" name="password_check">
+  <div class="field is-two-fifths">
+    <label class="label" for="email">E-mail Address:</label> 
+    <div class="control">
+      <input class="input" type="text" id="email" name="email">
+    </div>
+  </div>
+  
+  <div class="field is-two-fifths">
+    <label class="label" for="password">Create Password</label> 
+    <div class="control">
+      <input class="input" type="password" id="password" name="password">
+    </div>
+  </div>
+  
+  <div class="field is-two-fifths">
+    <label class="label" for="password_check">Confirm Password</label> 
+    <div class="control">
+      <input class="input" type="password" id="password_check" name="password_check">
+    </div>
+  </div>
 
   <?php 
     $crypto = new Core_Model_Crypto;
@@ -44,6 +66,11 @@ if(isset($_SESSION[session_key]['user_id'])) // user is already logged in
   <input type="hidden" name="hmac_token" value="<?php echo $hmac_pair['message'] ?>">
   <input type="hidden" name="hmac_key" value="<?php echo $hmac_pair['hmac'] ?>">
 
-  <p><input name="" type="submit" value="Register"></p>
+  <div class="field is-two-fifths">
+    <div class="control">
+      <input class="button is-primary" name="" type="submit" value="Register">
+    </div>
+  </div>
 
 </form>
+</section>
