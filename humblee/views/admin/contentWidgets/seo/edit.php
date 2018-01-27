a386653a9dd1583a0684bdd2d6f29e41b32eb8bd
Micah J. Murray <micah.murray@gmail.com>
2018-01-27 16:39:53 +0000

    add content widget style/js files to the widget

--- humblee/views/admin/contentWidgets/seo/edit.php
+++ humblee/views/admin/contentWidgets/seo/edit.php
@@ -106,103 +106,17 @@
         </div>
                
 </section>
- 
-<style type="text/css">
-    #google_sample {
-        width: 560px; /* 600 less padding */
-        background-color: #fff;
-        height: auto;
-        padding: 10px 20px;
-        font-family: arial,sans-serif;
-    }
-        #google_sample_title {
-            font-size: 18px;
-            color: #1a0dab;
-            height: 24px;
-        }
-        #google_sample_url {
-            display: block;
-            font-size: 14px;
-            color: #006621;
-        }
-        #google_sample_description {
-            font-size: small;
-            color: #545454;
-        }
-</style> 
 
-<script type="text/javascript"> 
-/* Global $ */
-
-
-function shrinkToFit(field_id,max_width)
-{
-    var field = $("#"+field_id),
-        text = field.html();
-        
-        if( field.width() > max_width)
-        {
-            //shorten by dropping last word and try again
-            var lastIndex = text.lastIndexOf(" ");
-            var newText = text.substring(0, lastIndex);
-            console.log(field.width() +" of "+ max_width +": "+ text +"/"+ newText);
-            field.html( newText + " ...");
-            // this makes a horrible endless loop
-            //return shrinkToFit(field_id,max_width);
-        }
-        else
-        {
-            //it short enough, return true
-            return true;
-        }
-}
-
-function charCount(element)
-{
-  var current_len = element.val().length,
-		max_len = element.attr("maxlength"), 
-		content = element.val();
-	
-	if( current_len > max_len )
-	{		
-		element.val( content.substring(0,max_len));	
-		current_len = max_len;
-	}	
-
-	
-	$("#meta_description_count_label").html( current_len +" of "+max_len +" characters.");	
-}
-
-$(document).ready(function(){
-   $("#page_title").on("keyup",function(){
-       var title_val = $(this).val();
-       $("#google_sample_title").html(title_val);
-       shrinkToFit('google_sample_title',300);
-   });
-   
-   $("#meta_description").on("keyup",function(){
-       var desc_val = $(this).val();
-       $("#google_sample_description").html(desc_val);
-   });
-   
-   $(".lengthcount").each(function(){
-		charCount( $(this));
-		$(this).keyup( function(){ charCount( $(this) ) } )
-	});
-});
-
-
-function openKCFinder(field) {
-    window.KCFinder = {
-        callBack: function(url) {
-            field.value = url;
-            $("#og_image_preview").attr('src',url).fadeIn('fast');
-            window.KCFinder = null;
-        }
-    };
-    window.open('<?php echo _app_path ?>core/libs/kcfinder/browse.php?type=images&dir=images', 'og_image',
-        'status=0, toolbar=0, location=0, menubar=0, directories=0, ' +
-        'resizable=1, scrollbars=0, width=800, height=600'
-    );
-}
-</script>
+<?php 
+    /**
+     * because the .css and .js code for this widget don't live in the /public folder
+     * they need to be included in-line in this file
+     */
+    echo '<style type="text/css">';
+    include('edit.css');
+    echo '</style>';
+    
+    echo '<script type="text/javascript">';
+    include('edit.js');
+    echo '</script>';
+?>
