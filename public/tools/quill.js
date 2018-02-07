c7aab42dbb23962bf5aba80a2127213f0646bca9
Micah J. Murray <micah.murray@gmail.com>
2018-02-07 02:32:52 +0000

    messing with quill

--- public/tools/quill.js
+++ public/tools/quill.js
@@ -2,6 +2,37 @@
 
 $("head").append('<link href="https://cdn.quilljs.com/1.3.5/quill.snow.css" rel="stylesheet">');
 
+class Counter {
+  constructor(quill, options) {
+    this.quill = quill;
+    this.options = options;
+    this.container = document.querySelector(options.container);
+    quill.on('text-change', this.update.bind(this));
+    this.update();  // Account for initial contents
+  }
+
+  calculate() {
+    let text = this.quill.getText();
+    if (this.options.unit === 'word') {
+      text = text.trim();
+      // Splitting empty text returns a non-empty array
+      return text.length > 0 ? text.split(/\s+/).length : 0;
+    } else {
+      return text.length;
+    }
+  }
+  
+  update() {
+    var length = this.calculate();
+    var label = this.options.unit;
+    if (length !== 1) {
+      label += 's';
+    }
+    this.container.innerText = length + ' ' + label;
+  }
+}
+
+
 var quill = new Quill('#edit_content', {
     theme: 'snow',
     imgHandler: 'selectImage',
@@ -9,11 +40,16 @@ var quill = new Quill('#edit_content', {
         toolbar: [
           ['bold', 'italic'],
           ['link', 'blockquote', 'code-block', 'image'],
-          [{ list: 'ordered' }, { list: 'bullet' }]
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ size: [ 'small', false, 'large', 'huge' ]}]
         ]
     }
 });
 quill.getModule("toolbar").addHandler("image", selectImage);
+
+quill.on('editor-change',function(){
+    $("#content").val(JSON.stringify(quill.getContents()));  
+});
 
 var insertPointIndex = 0;
 
@@ -28,11 +64,3 @@ function selectImage()
 function handleMediaManagerSelect(fileData){
     quill.insertEmbed(insertPointIndex, 'image', fileData.url);
 }
-
-$(document).ready(function(){
-//this doesn't work
-    $("#edit_content").on("change",function(){
-        $("#content").val(JSON.stringify(quill.getContents()));        
-    });
-
-})
