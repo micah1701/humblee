ebe2adf887042e8f3dba2340e622de631fa8af20
Micah Murray <micah.murray@gmail.com>
2026-05-30 19:39:52 -0400

    use dynamic, domain-based, env file

--- humblee/configuration/config.php
+++ humblee/configuration/config.php
@@ -1,5 +1,5 @@
 <?php
-define('include_only','scripts checking for this definition should die() if not set');
+define('include_only', 'scripts checking for this definition should die() if not set');
 
 /**
  * Set which configuration file to use
@@ -8,25 +8,26 @@ define('include_only','scripts checking for this definition should die() if not 
  * $environment = ($_SERVER['HTTP_HOST'] == "my-domain.com") ? 'env_production.php' : 'env_dev.php';
  *
  */
-$environment  = 'env_dev.php';
+$environment  = ($_SERVER['HTTP_HOST'] == "humblee.app") ? 'env_prod.php' : 'env_dev.php';
+
 
 // load the environment set above
 $_ENV['config'] = require_once $environment;
 
 // all $_SESSION info used by the framework is in a sub array of $_SESSION
 // default value is "humblee", eg $_SESSION['humblee']['my_variable'];
-define("session_key","humblee"); // example usage: echo $_SESSION[session_key]['user_id'];
+define("session_key", "humblee"); // example usage: echo $_SESSION[session_key]['user_id'];
 
 // tables used in this application
-define("_table_pages","humblee_pages");
-define("_table_content","humblee_content");
-define("_table_content_p13n","humblee_content_p13n");
-define("_table_content_types","humblee_content_types");
-define("_table_templates","humblee_templates");
-define("_table_users","humblee_users");
-define("_table_roles","humblee_roles");
-define("_table_user_roles","humblee_user_roles");
-define("_table_validation","humblee_validation");
-define("_table_accesslog","humblee_accesslog");
-define("_table_media","humblee_media");
-define("_table_media_folders","humblee_media_folders");
+define("_table_pages", "humblee_pages");
+define("_table_content", "humblee_content");
+define("_table_content_p13n", "humblee_content_p13n");
+define("_table_content_types", "humblee_content_types");
+define("_table_templates", "humblee_templates");
+define("_table_users", "humblee_users");
+define("_table_roles", "humblee_roles");
+define("_table_user_roles", "humblee_user_roles");
+define("_table_validation", "humblee_validation");
+define("_table_accesslog", "humblee_accesslog");
+define("_table_media", "humblee_media");
+define("_table_media_folders", "humblee_media_folders");
