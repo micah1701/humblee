cf265d646c20a6323abcf5c18921e9069e9ea717
Micah Murray <micah.murray@gmail.com>
2026-06-11 16:01:01 -0400

    remove content class which limits width at desktop

--- humblee/views/admin/templates/blank.php
+++ humblee/views/admin/templates/blank.php
@@ -1,4 +1,5 @@
 <?php
+
 /** @var string $userTheme */
 $userTheme ??= 'light';
 /** @var string $extra_head_code */
@@ -7,40 +8,50 @@ $extra_head_code ??= '';
 $template_view ??= false;
 ?>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title><?php echo  $_ENV['config']['domain'] ?> CMS</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta charset="utf-8">
+    <title><?php echo  $_ENV['config']['domain'] ?> CMS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-<?php
-use Humblee\Model\Crypto;
-$crypto = new Crypto();
-$hmac_pair = $crypto->get_hmac_pair();
-?>
+    <?php
 
-<link rel="stylesheet" type="text/css" href="<?php echo _app_path ?>node_modules/bulma/css/bulma.css">
-<link rel="stylesheet" type="text/css" href="<?php echo _app_path ?>humblee/css/admin/theme-<?php echo $userTheme ?? 'light' ?>.css">
-<link rel="stylesheet" type="text/css" href="<?php echo _app_path ?>node_modules/bulma-tooltip/dist/css/bulma-tooltip.min.css">
-<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    use Humblee\Model\Crypto;
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    $crypto = new Crypto();
+    $hmac_pair = $crypto->get_hmac_pair();
+    ?>
 
-<script type="text/javascript">var  APP_PATH = "<?php echo  _app_path ?>", XHR_PATH = "<?php echo  _app_path ?>core-request/", CURRENT_THEME = "<?php echo $userTheme ?? 'light' ?>", hmac_key = "<?php echo $hmac_pair['hmac'] ?>", hmac_token = "<?php echo $hmac_pair['message'] ?>";</script>
-<script src="<?php echo  _app_path ?>humblee/js/admin/admin.js"></script>
-<script src="<?php echo  _app_path ?>humblee/js/admin/theme-toggle.js"></script>
-<?php echo (isset($extra_head_code) ) ? $extra_head_code : '' ?>
+    <link rel="stylesheet" type="text/css" href="<?php echo _app_path ?>node_modules/bulma/css/bulma.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo _app_path ?>humblee/css/admin/theme-<?php echo $userTheme ?? 'light' ?>.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo _app_path ?>node_modules/bulma-tooltip/dist/css/bulma-tooltip.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <script type="text/javascript">
+        var APP_PATH = "<?php echo  _app_path ?>",
+            XHR_PATH = "<?php echo  _app_path ?>core-request/",
+            CURRENT_THEME = "<?php echo $userTheme ?? 'light' ?>",
+            hmac_key = "<?php echo $hmac_pair['hmac'] ?>",
+            hmac_token = "<?php echo $hmac_pair['message'] ?>";
+    </script>
+    <script src="<?php echo  _app_path ?>humblee/js/admin/admin.js"></script>
+    <script src="<?php echo  _app_path ?>humblee/js/admin/theme-toggle.js"></script>
+    <?php echo (isset($extra_head_code)) ? $extra_head_code : '' ?>
 
 </head>
+
 <body>
     <section class="section">
-        <div id="content" class="container">
-        <?php echo (isset($template_view) ) ? $template_view : '' ?>
+        <div id="content">
+            <?php echo (isset($template_view)) ? $template_view : '' ?>
         </div>
     </section><!-- end "content" -->
 
     <div id="confirmationBox" class="modal">
         <div class="modal-background"></div>
-            <div class="modal-card">
+        <div class="modal-card">
             <header class="modal-card-head">
                 <p class="modal-card-title">Are you sure?</p>
             </header>
@@ -55,10 +66,11 @@ $hmac_pair = $crypto->get_hmac_pair();
                 </article>
             </section>
             <footer class="modal-card-foot">
-              <button id="confirmButton" class="button is-info">Confirm</button>
-              <button class="button cancel">Cancel</button>
+                <button id="confirmButton" class="button is-info">Confirm</button>
+                <button class="button cancel">Cancel</button>
             </footer>
         </div>
     </div><!-- end of deletePageConfirmation -->
 </body>
+
 </html>
