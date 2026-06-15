58d3b3fb461ae30d7b3c48c0f5f1ef7e113274d5
Micah Murray <micah.murray@gmail.com>
2026-06-15 17:04:30 -0400

    Homepage calls custom view in custom template

--- application/Controller/Homepage.php
+++ application/Controller/Homepage.php
@@ -4,15 +4,8 @@ namespace App\Controller;
 
 class Homepage extends Page
 {
-    function __construct()
+    public function view(): string
     {
-        parent::__construct();
-        $this->setTemplatePath('application/views/templates/bare.php');
-        $this->index([]);
-    }
-
-    public function index(array $context = []): string
-    {
-        return $this->render('application/views/homepage-static.php', $context);
+        return $this->render('application/views/homepage-static.php');
     }
 }
